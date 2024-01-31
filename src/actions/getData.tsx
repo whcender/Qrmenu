8acb1434a5662b612c549b9040a46ba04f510ad7
whcender <heilender31@gmail.com>
2024-01-31 21:42:01 +0300

    fixxxxxxx aq

--- src/actions/getData.tsx
+++ src/actions/getData.tsx
@@ -7,7 +7,7 @@ import { url } from "@/lib/url";
 // fetchProduct fonksiyonu
 export const fetchProduct = async (page: number, cat?: string) => {
     if (!cat) {
-        const response = await fetch(`http://localhost:3000/api/getProducts?page=${page}&limit=8`)
+        const response = await fetch(`https://qrmenu-omega.vercel.app/api/getProducts?page=${page}&limit=8`)
 
         const data = await response.json();
 
@@ -16,7 +16,7 @@ export const fetchProduct = async (page: number, cat?: string) => {
         ));
     }
 
-    const response = await fetch(`http://localhost:3000/api/getProducts?page=${page}&limit=8&category=${cat}`)
+    const response = await fetch(`https://qrmenu-omega.vercel.app/api/getProducts?page=${page}&limit=8&category=${cat}`)
 
     const data = await response.json();
 
