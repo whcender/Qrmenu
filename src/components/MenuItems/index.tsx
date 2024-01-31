a065a1a5396cd380b696f6953b6ffad88b898c0d
whcender <heilender31@gmail.com>
2024-02-01 01:23:36 +0300

    menu items url update

--- src/components/MenuItems/index.tsx
+++ src/components/MenuItems/index.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link';
 import { categoryType } from '@/types';
 import Image from 'next/image';
+import { url } from '@/lib/url';
 
 const getData = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/getCategory", {
+    const res = await fetch(`${url}/api/getCategory`, {
       cache: "no-store"
     });
 
