169f4c742a82ec22a7716d4e36f0918929142042
whcender <heilender31@gmail.com>
2024-02-04 01:08:55 +0300

    ananı skm

--- src/app/api/getProducts/route.ts
+++ src/app/api/getProducts/route.ts
@@ -13,14 +13,32 @@ export const GET = async (req: NextRequest) => {
         try {
             const products = await prisma.products.findMany({
                 where: {
-                    ...(cat ? { categoryname: cat } : {}),
+                    categoryname: cat
                 },
                 take: limit,
                 skip: (page - 1) * limit,
             });
             return new NextResponse(JSON.stringify(products), { status: 200 });
-
         } catch (err) {
+            console.log(err);
+            return new NextResponse(
+                JSON.stringify({ message: "Something went wrong!" }),
+                { status: 500 }
+            );
+        }
+    }
+
+    if(!cat){
+        try {
+            const products = await prisma.products.findMany({
+                take: limit,
+                skip: (page - 1) * limit, // Calculate the offset based on the page and limit
+            });
+
+
+            return new NextResponse(JSON.stringify(products), { status: 200 });
+        } catch (err) {
+            console.log(err);
             return new NextResponse(
                 JSON.stringify({ message: "Something went wrong!" }),
                 { status: 500 }
