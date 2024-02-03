b19debaa19e947fe54f16b431f4147d947d45235
whcender <heilender31@gmail.com>
2024-02-04 01:16:36 +0300

    düzelmiyor aq

--- src/actions/getData.tsx
+++ src/actions/getData.tsx
@@ -1,27 +1,65 @@
 "use server";
 
 import ProductCart from "@/components/ProductCart";
-import { oneProductType } from "@/types";
+import { oneProductType, productType } from "@/types";
+import { prisma } from "@/utils/connect";
 import { url } from "@/lib/url";
+
+
 
 // fetchProduct fonksiyonu
 export const fetchProduct = async (page: number, cat?: string) => {
-    if (!cat) {
-        const response = await fetch(`${url}/api/getProducts?page=${page}&limit=8`)
+    const limit = 8
+    if (cat) {
+        try {
+            const products = await prisma.products.findMany({
+                where: {
+                    categoryname: cat
+                },
+                take: limit,
+                skip: (page - 1) * limit,
+            });
+            return products.map((item: any, index: number) => (
+                <ProductCart key={item.id} products={item} />
+            ));
+        } catch (err) {
+            console.log(err);
 
-        const data = await response.json();
-
-        return data.map((item: oneProductType, index: number) => (
-            <ProductCart key={item.id} products={item} />
-        ));
+        }
     }
+    if (!cat) {
+        try {
+            const products = await prisma.products.findMany({
+                take: limit,
+                skip: (page - 1) * limit, // Calculate the offset based on the page and limit
+            });
 
-    const response = await fetch(`${url}/api/getProducts?page=${page}&limit=8&category=${cat}`)
+            return products.map((item: any, index: number) => (
+                <ProductCart key={item.id} products={item} />
+            ));
 
-    const data = await response.json();
+        } catch (err) {
+            console.log(err);
 
-    return data.map((item: oneProductType, index: number) => (
-        <ProductCart key={item.id} products={item} />
-    ));
-
+        }
+    }
 }
+
+
+// if (!cat) {
+//     const respon = await fetch(`http://localhost:3000/api/getProducts?page=${page}&limit=8`)
+
+//     const data = await respon.json();
+
+//     return data.map((item: oneProductType, index: number) => (
+//         <ProductCart key={item.id} products={item} index={index} />
+//     ));
+// }
+
+// const response = await fetch(`http://localhost:3000/api/getProducts?page=${page}&limit=8&category=${cat}`)
+
+// const data = await response.json();
+
+// return data.map((item: oneProductType, index: number) => (
+//     <ProductCart key={item.id} products={item} index={index} />
+// ));
