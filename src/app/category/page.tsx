6fff4b3c01085529fd5e33a52c605bb8d76b5d8e
whcender <heilender31@gmail.com>
2024-02-04 13:35:16 +0300

    by e

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,7 +1,34 @@
 import MenuItems from '@/components/MenuItems'
-import { fetchProduct } from "@/actions/getData"
 import LoadMore from '@/components/LoadMore'
+import ProductCart from '@/components/ProductCart'
+import { url } from '@/lib/url'
 
+
+export const fetchProduct = async (page: number, cat?: string) => {
+
+    if (!cat) {
+        try {
+            const res = await fetch(`${url}/api/getProducts?page=${page}&limit=8`, {
+            });
+
+            if (!res.ok) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+
+            const result = await res.json();
+
+            return result.map((item: any) => (
+                <ProductCart key={item.id} products={item} />
+            ));
+
+
+        } catch (error) {
+            console.error("Error fetching data:", error);
+            return [];
+        }
+
+    }
+}
 
 const page = async () => {
 
