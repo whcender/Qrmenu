a3165a492125077f0536342dfca1a3fa8438c7d9
whcender <heilender31@gmail.com>
2024-02-04 13:43:00 +0300

    olsun artık

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -2,43 +2,42 @@ import MenuItems from '@/components/MenuItems'
 import LoadMore from '@/components/LoadMore'
 import ProductCart from '@/components/ProductCart'
 import { url } from '@/lib/url'
+import { productType } from '@/types'
 
 
-const fetchProduct = async (page: number, cat?: string) => {
-
-    if (!cat) {
-        try {
-            const res = await fetch(`${url}/api/getProducts?page=${page}&limit=8`, {
-            });
-
-            if (!res.ok) {
-                throw new Error(`HTTP error! Status: ${res.status}`);
-            }
-
-            const result = await res.json();
-
-            return result.map((item: any) => (
-                <ProductCart key={item.id} products={item} />
-            ));
 
 
-        } catch (error) {
-            console.error("Error fetching data:", error);
-            return [];
+const getData = async (page: number) => {
+    try {
+        const res = await fetch(`${url}/api/getProducts?page=${page}&limit=8`, {
+            cache: "no-store",
+        });
+
+        if (!res.ok) {
+            throw new Error(`HTTP error! Status: ${res.status}`);
         }
 
+        const result = await res.json();
+
+        return result;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return [];
     }
 }
 
 const page = async () => {
 
-    const products = await fetchProduct(1)
+    const products: productType = await getData(1)
+
 
     return (
         <div>
             <MenuItems />
             <div className='mt-10 '>
-                {products}
+                {products.map((item: any) => (
+                    <ProductCart key={item.id} products={item} />
+                )) }
                 <LoadMore />
             </div>
         </div>
