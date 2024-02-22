4a0aa3e7b1661311dea01584bf4c847cf78ace0d
whcender <heilender31@gmail.com>
2024-02-22 14:18:55 +0300

    button product names change

--- src/components/admin/buttonProduct/index.tsx
+++ src/components/admin/buttonProduct/index.tsx
@@ -154,14 +154,14 @@ const index = (image: any) => {
         <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0  opacity-40 bg-gray-600" />
         <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
           <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
-            Kategoriyi Düzenle
+            Ürünü Düzenle
           </Dialog.Title>
           <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
             Değişiklikleri kaydetmek için "Değişiklikleri Kaydet" butonuna tıklayın. (Değiştirmek istemediğiniz alanları boş bırakabilirsiniz.)
           </Dialog.Description>
           <fieldset className="mb-[15px] flex items-center gap-5">
             <label className="text-violet11 w-[90px] text-right text-[15px]" htmlFor="name">
-              Kategori İsmi
+              Ürün İsmi
             </label>
             <input
               className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
