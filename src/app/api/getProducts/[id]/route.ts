8916948e1a4dd8fbfa1a590b0f6ad7962df6c587
whcender <heilender31@gmail.com>
2024-02-22 13:12:50 +0300

    force try

--- src/app/api/getProducts/[id]/route.ts
+++ src/app/api/getProducts/[id]/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/utils/connect";
 
-export const dynamic = 'force-dynamic';
 
 export const GET = async (
     req: NextRequest,
