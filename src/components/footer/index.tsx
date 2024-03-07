a3a5bc891fe71a8f96a4c55bf478dd456418d1f8
whcender <heilender31@gmail.com>
2024-03-07 19:55:04 +0300

    footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center gap-2'>
       <h2 className='mt-4 font-semibold font-osw text-center'>AFİYET <span className='text-red-700'>OLSUN</span> :)</h2>
+      <h4 className='flex gap-3 items-center font-osw '><img className='size-4' src="https://i.hizliresim.com/h2tzn2v.png" alt="arlanmedya" /><a className="underline" href="https://www.arlanqrmenu.com/">Arlan Medya</a> @2024</h4>
     </div>
   )
 }
