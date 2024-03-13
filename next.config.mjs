fe187af0480eee9e86a94078b9908cc3a817ec1e
whcender <heilender31@gmail.com>
2024-03-13 13:59:30 +0300

    all menu

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
               protocol: "https",
-              hostname: "firebasestorage.googleapis.com",
+              hostname: "firebasestorage.googleapis.com"
             },
           ], // Görüntülerin yüklenebileceği domainleri burada belirtin
     },
--- src/components/MenuItems/index.tsx
+++ src/components/MenuItems/index.tsx
@@ -11,11 +11,11 @@ const Index = async () => {
       const res = await fetch(`${url}/api/getCategory`, {
         cache: "no-store"
       });
-  
+
       if (!res.ok) {
         throw new Error(`HTTP error! Status: ${res.status}`);
       }
-  
+
       const result = await res.json();
       return result;
     } catch (error) {
@@ -24,17 +24,23 @@ const Index = async () => {
     }
   };
 
-  const category:categoryType = await getData();
+  const category: categoryType = await getData();
 
   return (
     <div>
       <div className={`w-full overflow-x-scroll whitespace-nowrap px-3 py-2 transition-all duration-200 `}>
         <ul className="flex gap-4">
+          <Link className="flex flex-col items-center justify-center" href={`/category`}>
+            <Text text={"Hepsi"} text2={"All"} />
+            <div className={`w-24 h-24 flex items-center justify-center`}>
+              <Image className='w-24 h-24 object-cover' src="/menu.webp" alt={"arlan medya hepsi"} width={100} height={100} loading='lazy' placeholder="blur" blurDataURL="https://i.hizliresim.com/74xpsgl.gif" />
+            </div>
+          </Link>
           {category.map((item) => (
             <Link className="flex flex-col items-center justify-center" href={`/category/${item.name}`} key={item.name}>
-              <Text text={item.name} text2={item.ename}/>
+              <Text text={item.name} text2={item.ename} />
               <div className={`w-24 h-24 flex items-center justify-center`}>
-                <Image className='w-24 h-24 object-cover' src={item.image} alt={item.name} width={ 100 } height={ 100 } loading='lazy' placeholder="blur" blurDataURL="https://i.hizliresim.com/74xpsgl.gif" />
+                <Image className='w-24 h-24 object-cover' src={item.image} alt={item.name} width={100} height={100} loading='lazy' placeholder="blur" blurDataURL="https://i.hizliresim.com/74xpsgl.gif" />
               </div>
             </Link>
           ))}
