4a8c12097ef0d5986437be6b0ab23af622e152cb
Fatih Aslan <82971212+whcender@users.noreply.github.com>
2024-03-15 14:29:26 +0300

    Update text.tsx

--- src/components/MenuItems/text.tsx
+++ src/components/MenuItems/text.tsx
@@ -8,7 +8,7 @@ const text = ({text, text2}: {text: any, text2: string}) => {
     const { value } = useLanStore()
 
   return (
-    <p className={`w-24 text-center font-semibold text-sm`}>{value ? text : text2}</p>
+    <p className={`w-24 text-center font-semibold text-sm capitalize`}>{value ? text : text2}</p>
 
   )
 }
