3c8afca6613cf008e7a57c3e9bf899762d9c6067
whcender <heilender31@gmail.com>
2024-04-17 02:18:08 +0300

    some düzenlemeler

--- src/components/ProductCart/index.tsx
+++ src/components/ProductCart/index.tsx
@@ -13,7 +13,7 @@ const index = ({ products }: { products: oneProductType }) => {
 
     return (
         <ul className={`flex flex-wrap gap-10 justify-center items-center px-1 mt-4 font-outfit`}>
-            <div className='flex items-center justify-center gap-3 border p-5  w-full rounded-lg shadow-lg relative' key={products.id}> {/* Anahtar olarak benzersiz bir değer kullanılmalı */}
+            <div className='flex items-center justify-center gap-3 border py-5 px-2  w-full rounded-lg shadow-lg relative' key={products.id}> {/* Anahtar olarak benzersiz bir değer kullanılmalı */}
                 <Image
                     className='rounded-lg w-36 h-36 shadow-lg object-cover'
                     src={products.image}
@@ -25,7 +25,7 @@ const index = ({ products }: { products: oneProductType }) => {
                     blurDataURL="https://i.hizliresim.com/74xpsgl.gif"
                 />
                 <div className='flex flex-col gap-4'>
-                    <p className='w-full  font-black text-lg capitalize'>{value ? products.name : products.ename}</p>
+                    <p className='w-full  font-black text-base capitalize'>{value ? products.name : products.ename}</p>
                     <p className='text-xs text-gray-600 font-semibold w-full '>{shortDescription}</p>
                     <p className='font-black text-gray-700 text-lg'><span className='text-xs font-black'>{products.price}</span>₺</p>
                 </div>
