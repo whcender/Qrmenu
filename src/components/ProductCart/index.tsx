2bbc3ddeb4cc2691c8dd97dfac30194be4f85cc7
whcender <heilender31@gmail.com>
2024-04-16 17:02:11 +0300

    image

--- src/components/ProductCart/index.tsx
+++ src/components/ProductCart/index.tsx
@@ -10,7 +10,7 @@ const index = ({ products }: { products: oneProductType }) => {
 
     return (
         <ul className={`flex flex-wrap gap-10 justify-center items-center px-1 mt-4`}>
-            <div className='flex flex-col items-center justify-center gap-3 border p-5  w-full rounded-lg shadow-lg relative' key={products.id}> {/* Anahtar olarak benzersiz bir değer kullanılmalı */}
+            <div className='flex items-center justify-center gap-3 border p-5  w-full rounded-lg shadow-lg relative' key={products.id}> {/* Anahtar olarak benzersiz bir değer kullanılmalı */}
                 <Image
                     className='rounded-lg w-36 h-36 shadow-lg object-cover'
                     src={products.image}
@@ -21,9 +21,11 @@ const index = ({ products }: { products: oneProductType }) => {
                     placeholder="blur"
                     blurDataURL="https://i.hizliresim.com/74xpsgl.gif"
                 />
-                <p className='w-44 text-center font-semibold text-sm capitalize'>{value ? products.name : products.ename}</p>
-                <p className='text-xs text-gray-600 font-medium w-[95%] text-center'>{value ? products.description : products.edescription}</p>
-                <p className='font-semibold text-gray-700 text-lg'><span className='text-xs font-bold'>{products.price}</span>₺</p>
+                <div className=''>
+                    <p className='w-44  font-semibold text-sm capitalize'>{value ? products.name : products.ename}</p>
+                    <p className='text-xs text-gray-600 font-medium w-[95%] '>{value ? products.description : products.edescription}</p>
+                    <p className='font-semibold text-gray-700 text-lg'><span className='text-xs font-bold'>{products.price}</span>₺</p>
+                </div>
             </div>
         </ul>
     )
