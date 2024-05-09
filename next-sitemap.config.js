cebc08071d78474eaabe2a46ef226ee28eaf5f37
whcender <heilender31@gmail.com>
2024-05-10 00:12:07 +0300

    roobt günel

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,16 +5,18 @@ module.exports = {
     siteUrl: 'https://qrmenu-demo.vercel.app', // Kendi sitenizin URL'si
     generateRobotsTxt: true, // (Opsiyonel) robots.txt dosyasını oluşturun
     robotsTxtOptions: {
-      policies: [
-        {
-          userAgent: '*',
-          allow: '/', // Tüm sayfalara izin ver
-          disallow: '/dashboard', // Sadece /dashboard sayfasını engelle
-        },
-      ],
-      additionalSitemaps: [
-        'https://qrmenu-demo.vercel.app/sitemap.xml', // Ek site haritalarını buraya ekleyin
-      ],
+        policies: [
+            {
+                userAgent: '*', // Tüm botlar için geçerli
+                allow: '/', // Tüm sayfalara izin ver
+            },
+            {
+                userAgent: '*',
+                disallow: '/dashboard', // Sadece /dashboard sayfasını engelle
+            },
+        ],
+        additionalSitemaps: [
+            'https://qrmenu-demo.vercel.app/sitemap.xml', // Ek site haritalarını buraya ekleyin
+        ],
     },
-  }
-  
+}
