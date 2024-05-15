c6e686f0df0056092f5f07ceb8edda26ffa66629
whc <82971212+whcender@users.noreply.github.com>
2024-05-15 13:20:16 +0300

    Update index.tsx

--- src/components/admin/button/index.tsx
+++ src/components/admin/button/index.tsx
@@ -121,7 +121,7 @@ const index =  (image: any ,mainNamee: any) => {
         try {
           const options = {
             maxSizeMB: 1, // Max dosya boyutu megabayt cinsinden
-            maxWidthOrHeight: 100, // Max genişlik veya yükseklik piksel cinsinden
+            maxWidthOrHeight: 200, // Max genişlik veya yükseklik piksel cinsinden
             useWebWorker: true,
           };
     
